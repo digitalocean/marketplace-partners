e02ec0a1420cef81275e55c98d5838192c497056
Antonio Rosales <arosales@noreply.com>
2019-12-06 01:04:18 -0700

    Add back revoked_keys operation.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -12,6 +12,8 @@ find /var/log -mtime -1 -type f -exec truncate -s 0 {} \;
 rm -rf /var/log/*.gz /var/log/*.[0-9] /var/log/*-????????
 rm -rf /var/lib/cloud/instances/*
 rm -f /root/.ssh/authorized_keys /etc/ssh/*key*
+touch /etc/ssh/revoked_keys
+chmod 600 /etc/ssh/revoked_keys
 
 # Securely erase the unused portion of the filesystem
 GREEN='\033[0;32m'
