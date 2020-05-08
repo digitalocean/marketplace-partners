8b0f82dc653d55f898df704d87fbdacbb4c9f29c
Jim <countingtoten@gmail.com>
2020-05-08 13:13:00 -0500

    Add Ubuntu 20.04 as an approve OS

--- scripts/img_check.sh
+++ scripts/img_check.sh
@@ -561,6 +561,8 @@ osv=0
 
 if [[ $OS == "Ubuntu" ]]; then
         ost=1
+    if [[ $VER == "20.04" ]]; then
+        osv=1
     if [[ $VER == "18.04" ]]; then
         osv=1
     elif [[ $VER == "16.04" ]]; then
