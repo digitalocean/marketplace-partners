b9f403d8f1b83b0e60d9128cc36638c2a6efc0c6
Cory Schwartz <cory@protocol.ai>
2021-04-02 13:21:50 -0700

    truncate bash history again

--- scripts/90-cleanup.sh
+++ scripts/90-cleanup.sh
@@ -27,6 +27,7 @@ fi
 
 rm -rf /tmp/* /var/tmp/*
 history -c
+cat /dev/null > /root/.bash_history
 unset HISTFILE
 find /var/log -mtime -1 -type f -exec truncate -s 0 {} \;
 rm -rf /var/log/*.gz /var/log/*.[0-9] /var/log/*-????????
